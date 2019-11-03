d42b971cad791c1dc222f6d92c42ed246af15418
ringsaturn <ringsaturn.me@gmail.com>
2019-11-03 17:20:19 +0800

    Update docs & fix test release

--- bin.sh
+++ bin.sh
@@ -4,9 +4,10 @@ SERVICE=$1
 if [ x"$SERVICE" = x"test"  ]; then
     poetry install
     exec poetry run pytest tests/
-elif [ x"$SERVICE" = x"tests"  ]; then
-    poetry install
-    exec poetry run pytest tests/
+elif [ x"$SERVICE" = x"test-release"  ]; then
+    rm -rf dist/
+    poetry config repositories.test https://test.pypi.org/legacy/
+    poetry publish --build --repository=test -vvv
 elif [ x"$SERVICE" = x"release" ]; then
     rm -rf dist/
     exec poetry publish --build
